00d6bad5344268894b3a51a48d9c18ec91556b5d
John Atkins <jdatkins@tql.com>
2021-05-18 16:02:47 -0400

    C# practice

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,30 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome back, Commander.");
+            var sarah = new Student();
+            sarah.Firstname = "Sarah";
+            sarah.Lastname = "George";
+            sarah.SetHireDate(2017, 6, 26);
+
+            var chuck = new Student();
+            chuck.Firstname = "Chuck";
+            chuck.Lastname = "Traylor";
+
+            var mattea = new Student();
+            mattea.Firstname = "Mattea";
+            mattea.Lastname = "Swain";
+
+            sarah.Print();
+            chuck.Print();
+            mattea.Print();
+
+            //var company = "TQL";
+            //var student = "John";
+            //var tenure = 7;
+
+            //Console.WriteLine($"Welcome back, Commander, from {company}!");
+            //Console.WriteLine($"My name is {student} and I have been at TQL for {tenure} months.");
+            //Console.WriteLine();
         }
     }
 }
---
+++ HelloWorld/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Student
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public bool Active { get; set; }
+        private DateTime HireDate { get; set; }
+
+        public void SetHireDate(int year, int month, int day)
+        {
+            if(year < 1950 || year > 2200)
+            {
+                Console.WriteLine($"Year must be between 1950 and 2200.");
+                return;
+            }
+            if(month < 1 || month > 12)
+            {
+                Console.WriteLine($"Do you know what a calendar is???? (Invalid month entered!)");
+                return;
+            }
+            if(day < 1 || day > 31)
+            {
+                Console.WriteLine($"Thirty days have September, April, June, and November. (Invalid day entered!)");
+            }
+            HireDate = new DateTime(year, month, day);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Firstname} {Lastname} - Hired: {HireDate.ToString("MM/dd/yyyy")}");
+
+        }
+    }
+}
